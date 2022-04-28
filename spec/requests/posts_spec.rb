e856776406b762092e977fa7d4b2419810cc3845
Billions Joel <joelatugonza@gmail.com>
2022-04-28 22:54:11 +0300

    Add test to check if body includes specific text

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,5 +11,8 @@ RSpec.describe 'Posts', type: :request do
     it 'should render the index template for posts' do
       expect(response).to render_template(:index)
     end
+    it "should have text 'Showing all posts for user with id:'" do
+    expect(response.body).to include('Showing all posts for user with id:')
+    end
   end
 end
