718956ade23bed9e391f437561f7f931a1e92292
Billions Joel <joelatugonza@gmail.com>
2022-04-28 18:29:26 +0300

    Fix linter

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
     before(:example) { get users_path }
-    it "should return response ok" do
+    it 'should return response ok' do
       expect(response).to have_http_status(:ok)
     end
-    it "should render the index template" do
+    it 'should render the index template' do
       expect(response).to render_template(:index)
     end
     it "should respond with 'Home page'" do
@@ -14,6 +14,6 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
   end
 end
