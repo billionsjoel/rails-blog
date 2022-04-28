d9268255c66c2925e1f0d72f2a430a0b290824e5
Billions Joel <joelatugonza@gmail.com>
2022-04-28 22:45:53 +0300

    Add test to check for ok status

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending 'add some examples (or delete) #{__FILE__}'
+  describe 'GET /users/:user_id/posts' do
+    before(:example) do
+      get user_posts_path(user_id:1)
+    end
+    it 'should respond with status ok' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
