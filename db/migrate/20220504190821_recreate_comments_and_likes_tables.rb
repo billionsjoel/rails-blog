385ce2675dfbae1b864c614ac0d83b9b465406a5
Billions Joel <joelatugonza@gmail.com>
2022-05-04 22:13:44 +0300

    Create references

--- db/migrate/20220504190821_recreate_comments_and_likes_tables.rb
+++ db/migrate/20220504190821_recreate_comments_and_likes_tables.rb
@@ -2,5 +2,12 @@ class RecreateCommentsAndLikesTables < ActiveRecord::Migration[7.0]
   def change
     drop_table :comments
     drop_table :likes
+    create_table :comments do |t|
+      t.references :author, null: false, foreign_key: {to_table: :users}
+      t.references :post, null: false, foreign_key: true
+      t.text :text
+
+      t.timestamps
+    end
   end
 end
