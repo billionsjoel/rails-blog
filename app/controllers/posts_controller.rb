c3579a842bde0511b78c92c2c755990a3469b03f
Billions Joel <joelatugonza@gmail.com>
2022-05-06 01:42:18 +0300

    Add create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,4 +14,15 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:title,:text))
+    @post.author_id = params[:user_id]
+    if @post.save
+      flash[:success]='Post created Successfully'
+      redirect_to user_posts_url
+    else
+       @post.errors.full_messages
+      render :new
+    end
+  end
 end
