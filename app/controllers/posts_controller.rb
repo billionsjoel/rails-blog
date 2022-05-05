0602d44b42a6dd5f1de8b1540b592476ac8a2315
Billions Joel <joelatugonza@gmail.com>
2022-05-05 14:42:41 +0300

    Show recent comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,9 @@ class PostsController < ApplicationController
     @user = current_user
     @posts = current_user.last_three_posts
   end
+
+  def show
+      @post = Post.find params[:id]
+      @comments = @post.recent_comments
+  end
 end
