65eab1e2506c4dfccf149e5b7acd30fc56ee8a6a
Billions Joel <joelatugonza@gmail.com>
2022-05-05 08:17:07 +0300

    Update likes and posts counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,15 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def update_post_comment_counter(value)
-    post = Post.where('id = ?', self.post_id).first
-    post.comments_counter = value
-    post.save
+  def update_post_comment_counter
+    post.increment!(:comments_counter)
   end
 
-  def update_post_likes_counter(value)
-    post = Post.where('id = ?', self.post_id).first
-    post.likes_counter = value
-    post.save
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
   end
 end
