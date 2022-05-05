19b3f51b5db0c8581a6a98d8b51c248b688ba0c2
Billions Joel <joelatugonza@gmail.com>
2022-05-05 08:02:31 +0300

    Update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_comment_counter(value)
+    post = Post.where('id = ?',self.post_id).first
+    post.comments_counter = value
+    post.save
+  end
 end
