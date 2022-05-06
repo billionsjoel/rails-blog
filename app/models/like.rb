40211000e7d72d767d0c67b5831df62d6fc1776f
Billions Joel <joelatugonza@gmail.com>
2022-05-06 03:27:02 +0300

    Fix likes counter attribute

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: :likes_counter
-  belongs_to :post
-  after_save :update_post_likes_counter
-
-  private
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, counter_cache: :likes_counter
 
   def update_post_likes_counter
     post.increment!(:likes_counter)
