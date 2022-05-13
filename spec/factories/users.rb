676e884c0065db64b72c2b598de18fd7607ea42a
Billions Joel <joelatugonza@gmail.com>
2022-05-13 21:41:40 +0300

    Fix linter

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name { "Name" }
-    photo { "MyString" }
-    bio { "My Bio" }
-    email { "name@gmail.com" }
-    role { "user" }
-    password { "MyPassword" }
-    password_confirmation { "MyPassword" }
+    name { 'Name' }
+    photo { 'MyString' }
+    bio { 'My Bio' }
+    email { 'name@gmail.com' }
+    role { 'user' }
+    password { 'MyPassword' }
+    password_confirmation { 'MyPassword' }
     factory :admin do
-      role { "admin" }
+      role { 'admin' }
     end
   end
 end
