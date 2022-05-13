b219ec86fc48899e124df1656e4e27dffcd06781
Billions Joel <joelatugonza@gmail.com>
2022-05-13 08:45:52 +0300

    Fix linter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @users = User.all
@@ -9,15 +9,19 @@ class UsersController < ApplicationController
     user = User.new params
     user.save
   end
+
   def update
     user = User.new params
     user.save
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   private
+
   def user_params
-     params.require(:user).permit(:name,:password,:password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
